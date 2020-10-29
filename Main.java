cf899bd3c5f56367d5b7d74a4cf8771f4895c513
unknown <sindhuk.xworkz@gmail.com>
2020-10-29 16:07:19 +0530

    FunctionalInterface

---
+++ Main.java
@@ -0,0 +1,41 @@
+package xworkz.com.FunctionalinterfaceExamplde;
+
+//package xworkz.com.FunctionalinterfaceExamplde;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        String filename = "xxx";
+        Person p = new Person("Sindhu", "kumar");
+
+        // save the object to file
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream(filename);
+            out = new ObjectOutputStream(fos);
+            out.writeObject(p);
+
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // read the object from file
+        // save the object to file
+        FileInputStream fis = null;
+        ObjectInputStream in = null;
+        try {
+            fis = new FileInputStream(filename);
+            in = new ObjectInputStream(fis);
+            p = (Person) in.readObject();
+            in.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println(p);
+    }
+}
